2f8c1c2e6d1912d58cf62b6ffacfe15ca219b380
dasom222g <dasom228@gmail.com>
2026-09-16 13:30:19 +0900

    컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import Greeting from "./Greeting";
+
 function App() {
+  // logic
+  // 구조분해할당 - object destructuring
+  const animals = ["dog", "cat", "pig"];
+  // const second = animals[1]
+
+  const [ second, first ] = animals
+  
+  console.log("🚀 first:", first) // dog
+
+  console.log("🚀 second:", second)
+
+  const todoItem = {
+    todo: "React학습하기",
+    isComplete: false
+  }
+
+  const { todo: todoName } = todoItem
+  console.log("🚀 ~ App ~ todo:", todoName)
+
+
+  // view
   return (
     <div className="App">
       변경~!!
+      <Greeting />
+      <Greeting />
+      {/* 미션: Card컴포넌트 만들고, 이 자리에서 호출 */}
     </div>
   );
 }
---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div style={{ "backgroundColor": "pink", "padding": "10px" }}>
+      <p>안녕하세요, 홍진호님!</p>
+      <p>오늘도 좋은하루 보내세요😊</p>
+    </div>
+  )
+}
+
+export default Greeting
